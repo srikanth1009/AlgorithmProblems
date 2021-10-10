a5f10b7b76ddb41d80470bcefad36dad3c4b1b45
srikanth1009 <srikanthreddyyaddala8@gmail.com>
2021-10-10 10:56:40 +0530

    Added:Programs for datastructures algorithm problems

---
+++ src/main/java/com/bridgelabz/algorithm/BubbleSort.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.algorithm;
+public class BubbleSort {
+	  public static String[] sort(String array[], int length) {
+	        String temp = " ";
+	        for (int i = 0; i < length; i++) {
+	            for (int j = i + 1; j < length; j++) {
+	                if (array[i].compareToIgnoreCase(array[j]) > 0) {
+	                    temp = array[i];
+	                    array[i] = array[j];
+	                    array[j] = temp;
+	                }
+	            }
+	        }
+	        return array;
+	    }
+
+	    public static void main(String[] args) {
+	        String[] arr = { "cat", "dog", "lion", "elephant", "mouse", "tiger" };
+	        String sortedArray[] = sort(arr, arr.length);
+	        for (int i = 0; i < sortedArray.length; i++) {
+	            System.out.println(sortedArray[i]);
+	        }
+	    }
+}
